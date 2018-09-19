41a398cf0d7a597508f11303dc336b2b4bab998a
Anthony Weathersby <anthonycweathersby@gmail.com>
2018-09-19 11:23:59 -0600

    Deleted anim.jsqq

--- source/root/anim.js
+++
@@ -1,9 +0,0 @@
-import { Animation } from "../animation/animation";
-import { TransformTo } from "../animation/transformto";
-import { Transitioneer } from "../animation/transitioneer";
-
-const anim = Animation;
-anim.transformTo = TransformTo;
-anim.transitioneer = Transitioneer;
-
-export default anim;
