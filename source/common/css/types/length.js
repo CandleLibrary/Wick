670e88d6ea3a8a2648a8ae52af312f3be4a29c3f
Anthony Weathersby <anthonycweathersby@gmail.com>
2018-11-02 17:47:02 -0600

    Added new length types

--- source/common/css/types/length.js
+++ source/common/css/types/length.js
@@ -1,18 +1,17 @@
-import { Lexer } from "../../string_parsing/lexer";
+import {
+    Lexer
+} from "../../string_parsing/lexer";
 export class CSS_Length extends Number {
     static _parse_(l, rule, r) {
         let tx = l.tx,
             pky = l.pk.ty;
-
         if (l.ty == l.types.num || tx == "-" && pky == l.types.num) {
             let mult = 1;
-
             if (l.ch == "-") {
                 mult = -1;
                 tx = l.p.tx;
                 l.p.n();
             }
-
             if (l.p.ty == l.types.id) {
                 let id = l.sync().tx;
                 l.n();
@@ -23,23 +22,43 @@ export class CSS_Length extends Number {
     }
 
     static _verify_(l) {
-        if (typeof(l) == "string" && !isNaN(parseInt(l)) && !l.includes("%"))
-            return true;
+        if (typeof(l) == "string" && !isNaN(parseInt(l)) && !l.includes("%")) return true;
         return false;
     }
 
     constructor(v, u = "") {
-
+        
         if (typeof(v) == "string") {
             let lex = new Lexer(v);
             let val = CSS_Length._parse_(lex);
-            if (val) 
-                return val;
+            if (val) return val;
+        }
+
+        if(u){
+            switch(u){
+                //Absolute
+                case "px": return new PXLength(v);
+                case "mm": return new MMLength(v);
+                case "cm": return new CMLength(v);
+                case "in": return new INLength(v);
+                case "pc": return new PCLength(v);
+                case "pt": return new PTLength(v);
+                //Relative
+                case "ch": return new CHLength(v);
+                case "em": return new EMLength(v);
+                case "ex": return new EXLength(v);
+                case "rem": return new REMLength(v);
+
+                //View Port
+                case "vh": return new VHLength(v);
+                case "vw": return new VWLength(v);
+                case "vmin": return new VMINLength(v);
+                case "vmax": return new VMAXLength(v);
+
+            }
         }
 
         super(v);
-
-        this.unit = u;
     }
 
     get milliseconds() {
@@ -47,7 +66,6 @@ export class CSS_Length extends Number {
             case ("s"):
                 return parseFloat(this) * 1000;
         }
-
         return parseFloat(this);
     }
 
@@ -67,7 +85,53 @@ export class CSS_Length extends Number {
         return new CSS_Length(this + (to - this) * t, this.unit);
     }
 
-    copy(other){
+    copy(other) {
         return new CSS_Length(other, this.unit);
     }
+
+    set unit(t){};
+    get unit(){return ""}
+}
+
+export class PXLength extends CSS_Length {
+    get unit(){return "px"}
+}
+export class MMLength extends CSS_Length {
+    get unit(){return "mm"}
+}
+export class CMLength extends CSS_Length {
+    get unit(){return "cm"}
+}
+export class INLength extends CSS_Length {
+    get unit(){return "in"}
+}
+export class PTLength extends CSS_Length {
+    get unit(){return "pt"}
+}
+export class PCLength extends CSS_Length {
+    get unit(){return "pc"}
+}
+export class CHLength extends CSS_Length {
+    get unit(){return "ch"}
+}
+export class EMLength extends CSS_Length {
+    get unit(){return "em"}
+}
+export class EXLength extends CSS_Length {
+    get unit(){return "ex"}
+}
+export class REMLength extends CSS_Length {
+    get unit(){return "rem"}
+}
+export class VHLength extends CSS_Length {
+    get unit(){return "vh"}
+}
+export class VWLength extends CSS_Length {
+    get unit(){return "vw"}
+}
+export class VMINLength extends CSS_Length {
+    get unit(){return "vmin"}
+}
+export class VMAXLength extends CSS_Length {
+    get unit(){return "vmax"}
 }
