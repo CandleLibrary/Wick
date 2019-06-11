a2990ccac8e324c70ac7230e3052ecdfb5ca63fe
galactrax <anthonycweathersby@gmail.com>
2019-06-10 21:26:45 -0600

    added link processing code to link.mjs

--- source/compiler/html/link.mjs
+++ source/compiler/html/link.mjs
@@ -5,4 +5,10 @@ export default class a extends ElementNode{
 	constructor(env, tag, children, attribs, presets){
 		super(env, "a", children, attribs, presets);
 	}
+
+	createElement(){
+        const element = document.createElement("a");
+        this.presets.processLink(element);
+        return element;
+    }
 }
