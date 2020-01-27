bc72981ab346f1ecbde34e7d801f8fa71819b5ee
galactrax <anthonycweathersby@gmail.com>
2020-01-27 08:26:45 -0700

    adjusting svg html node to make sure svg elements are correctly created

--- source/compiler/html/svg.js
+++ source/compiler/html/svg.js
@@ -3,6 +3,11 @@ import ElementNode from "./element.js";
 
 export default class svg extends ElementNode{
 	constructor(env, tag, children, attribs, presets){
-		super(env, "svg", children, attribs, presets);
+		super(env, tag, children, attribs, presets);
+	}
+
+
+	createElement(){
+		return document.createElementNS("http://www.w3.org/2000/svg",this.tag);
 	}
 }
