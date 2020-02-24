167254b9f97f03cf71a3d7ccd666cee408843de8
galactrax <anthonycweathersby@gmail.com>
2020-02-24 09:21:17 -0700

    exposing compiler prototype for NodeJS

--- source/wick.node.mjs
+++ source/wick.node.mjs
@@ -4,8 +4,12 @@ import lite from "./lite/tools.js";
 
 import stamp from "./stamp/stamp.js";
 
+import component from "./compiler/component_prototype.js";
+
 wick.lite = lite;
 
 wick.stamp = stamp; //Compiles wick component into standalone components. 
+
+wick.component_prototype = component.prototype;
 
 export default wick;
