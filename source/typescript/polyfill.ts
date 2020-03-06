b1c5212cf31d0c443bf08d9b74f67ebea32dc89d
galactrax <anthonycweathersby@gmail.com>
2020-03-06 08:10:42 -0700

    Add no-check

--- source/typescript/polyfill.ts
+++ source/typescript/polyfill.ts
@@ -1,11 +1,9 @@
+//@ts-nocheck
 import HTML from "@candlefw/html";
-//import CSS from "@candlefw/css";
-
-export default function(){
-    if (typeof(global) !== "undefined") {
-    	HTML.polyfill();
-    	//CSS.polyfill();
-
-        global.window = global.window || {}
+export default function () {
+    if (typeof (global) !== "undefined") {
+        HTML.polyfill();
+        //CSS.polyfill();
+        global.window = global.window || {};
     }
 }
