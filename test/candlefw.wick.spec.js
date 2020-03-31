b3dd3f9828b4f0d8d3724d5e28b053f4ed39ec90
galactrax <anthonycweathersby@gmail.com>
2020-03-30 18:13:09 -0600

    Add new cfw.test test file

---
+++ test/candlefw.wick.spec.js
@@ -0,0 +1,13 @@
+import { wick, parser } from "../build/library/wick.js";
+
+"@candlefw/wick test spec";
+"PARSER"; "#";
+{
+    {
+        "TODO: Test wick"; "#";
+        ((!parser("test")));
+    }
+}
+
+
+{ "TODO: Test parser"; "#"; ((parser)); }
