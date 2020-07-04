28bf3c16c8fc091c27f60398bf544fb4b22669fd
galactrax <anthonycweathersby@gmail.com>
2020-07-04 14:47:50 -0600

    Remove custom components from presets
    Unfreeze presets.model member

--- source/typescript/presets.ts
+++ source/typescript/presets.ts
@@ -1,8 +1,8 @@
 import URL from "@candlefw/url";
 
 import { PresetOptions } from "./types/preset_options.js";
-import { WickRTComponent } from "./runtime/runtime_component.js";
 import { ExtendedComponent } from "./types/extended_component.js";
+import { Component } from "./types/types.js";
 
 let CachedPresets = null;
 
@@ -31,8 +31,8 @@ export default class Presets implements PresetOptions {
      */
     components?: Map<string, any>;
 
-    custom_components?: {};
     component_class: Map<string, any>;
+
     component_class_string: Map<string, string>;
 
     schemes?: {};
@@ -47,8 +47,7 @@ export default class Presets implements PresetOptions {
 
     wrapper: ExtendedComponent;
 
-    named_components: Map<string, typeof WickRTComponent>;
-
+    named_components: Map<string, Component>;
 
     static global = { get v() { return CachedPresets; }, set v(e) { } };
 
@@ -78,8 +77,6 @@ export default class Presets implements PresetOptions {
 
         this.component_class_string = new Map;
 
-        this.custom_components = {};
-
         this.schemes = {};
 
         this.models = {};
@@ -96,23 +93,6 @@ export default class Presets implements PresetOptions {
                 //    console.log(`Unexpected option [${cn}]`);
             }
 
-
-        // const d = preset_options.components;
-        // if (d)
-        //     for (const cn in d)
-        //         this.components[cn] = c[cn];
-
-        // const e = preset_options.custom_scopes;
-        // if (e)
-        //     for (const cn in e)
-        //         if (cn instanceof CustomComponent)
-        //             this.custom_scopes[cn] = c[cn];
-
-        c = preset_options.custom_components;
-        if (c)
-            for (const cn in c)
-                this.custom_components[cn] = c[cn];
-
         c = preset_options.models;
 
         if (c)
@@ -125,17 +105,13 @@ export default class Presets implements PresetOptions {
             for (const cn in c)
                 this.schemes[cn] = c[cn];
 
-        //this.options.USE_SHADOW = (preset_options.options.USE_SHADOW)
-        //    ? (DOC.head.createShadowRoot
-        //        || DOC.head.attachShadow) : false;
         this.options.USE_SHADOWED_STYLE = ((preset_options.options.USE_SHADOWED_STYLE) && (this.options.USE_SHADOW));
 
         this.url = new URL;
 
         Object.freeze(this.options);
-        //Object.freeze(this.custom_scopes);
         Object.freeze(this.schemes);
-        Object.freeze(this.models);
+        //Object.freeze(this.models);
 
         CachedPresets = this;
     }
