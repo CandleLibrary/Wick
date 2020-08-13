d2912e9b113378279c40405b38a1658460d46753
Anthony Weathersby <anthonycweathersby@gmail.com>
2020-08-13 09:52:35 -0600

    Modify node extension type

--- source/typescript/types/extended_component.ts
+++ source/typescript/types/extended_component.ts
@@ -1,6 +1,7 @@
 import { WickComponentErrorStore } from "./errors.js";
 import { WickRTComponent } from "../runtime/runtime_component.js";
 import { Component } from "./types.js";
+import { RuntimeComponent } from "../wick.js";
 /**
  * A compiled component that can be mounted to a DOM node.
  */
@@ -8,6 +9,9 @@ interface Extension {
     errors: WickComponentErrorStore;
     pending: Promise<Extension & Component>;
     mount: (data?: object, ele?: HTMLElement) => Promise<WickRTComponent>;
+    class: typeof RuntimeComponent;
+    classWithIntegratedCSS: typeof RuntimeComponent;
+    class_string: string;
 }
 
 export type ExtendedComponent = (Component & Extension);
