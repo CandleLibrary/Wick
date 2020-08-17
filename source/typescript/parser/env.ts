8f9c867c3729bb9b590bb015a50932ca2ca65c05
Anthony Weathersby <anthonycweathersby@gmail.com>
2020-08-17 10:00:19 -0600

    Implement new error handlers to match signature of ParserEnvironment
    Match type found in cfw/hydrocarbon@0.4.0-dev

--- source/typescript/parser/env.ts
+++ source/typescript/parser/env.ts
@@ -7,7 +7,7 @@ import {
     CSSNodeTypeLU
 } from "@candlefw/css";
 
-import { ParserEnvironment } from "@candlefw/hydrocarbon/build/library/runtime.js";
+import { ParserEnvironment } from "@candlefw/hydrocarbon";
 import { Lexer } from "@candlefw/wind";
 import { JSParserEnvironment, JSParserEnv, JSNodeTypeLU } from "@candlefw/js";
 
@@ -59,73 +59,27 @@ const env = <WickParserEnvironment>{
             env.addParseError(`Unexpected token [${tk}]`, lex, env.url);
         },
 
-        defaultError: (tk, env: ParserEnvironment & { ASI: boolean; }, output, lex, prv_lex, ss, lu) => {
-            if (lex.tx == "//" || lex.tx == "/*") {
-                if (lex.tx == "//") {
-                    while (!lex.END && lex.ty !== lex.types.nl)
-                        lex.next();
+        frrh: (tk, env: ParserEnvironment & { ASI: boolean; }, output, lex, prv_lex, ss, lu) => {
+            const val = JSParserEnvironment.functions.frrh(tk, env, output, lex, prv_lex, ss, lu);
 
-                    if (lex.END) {
-                        lex.tl = 0;
-                        return 0;//lu({ tx: ";" });
-                    }
-                } else
-                    if (lex.tx == "/*") {
-                        //@ts-ignore
-                        while (!lex.END && (lex.tx !== "*" || lex.pk.tx !== "/"))
-                            lex.next();
-                        lex.next(); //"*"
+            if (val >= 0) return val;
 
-                    }
+            return -1;
+        },
 
-                return lu(lex.next());
-            }
+        lrrh: (tk, env: ParserEnvironment & { ASI: boolean; }, output, lex, prv_lex, ss, lu) => {
+            const val = JSParserEnvironment.functions.lrrh(tk, env, output, lex, prv_lex, ss, lu);
 
+            if (val >= 0) return val;
 
-            /*USED for Automatic Semicolon Insertion*/
-            if (env.ASI && lex.tx !== ")" && !lex.END) {
-
-                let ENCOUNTERED_END_CHAR = (lex.tx == "}" || lex.END);
-
-                while (!ENCOUNTERED_END_CHAR && !prv_lex.END && prv_lex.off < lex.off) {
-                    prv_lex.next();
-                    if (prv_lex.ty == prv_lex.types.nl)
-                        ENCOUNTERED_END_CHAR = true;
-                }
-
-                if (lex.tx == "</") // As in "<script> script body => (</)script>"
-                {
-                    lex.tl = 0;
-                    return lu(<Lexer>{ tx: ";" });
-                }
-
-
-                if (ENCOUNTERED_END_CHAR) {
-                    lex.tl = 0;
-                    return lu(<Lexer>{ tx: ";" });
-                }
-            }
-
-            if (lex.ty == lex.types.sym && lex.tx.length > 1) {
-                //Try splitting up the symbol
-                lex.tl = 0;
-
-                lex.next(lex, false);
-                return lu(lex);
-            }
-
-            if (lex.END) {
-                lex.tl = 0;
-                return lu(<Lexer>{ tx: ";" });
-            }
-        }
+            return -1;
+        },
     },
 
     options: {
         integrate: false,
 
         onstart: () => {
-
             //env.table = {};
             env.ASI = true;
         }
