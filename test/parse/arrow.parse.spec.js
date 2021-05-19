d977c22480996e97e0e98265b2c893d6d4c8461b
Anthony Weathersby <acweathersby.codes@gmail.com>
2021-05-19 16:50:10 -0600

    Chore: Modify test

--- test/parse/arrow.parse.spec.js
+++ test/parse/arrow.parse.spec.js
@@ -17,7 +17,7 @@ import { createClassStringObject } from "../../build/library/component/render/js
 import { renderCompressed } from "@candlefw/js";
 
 
-const source =`
+const source = `
 var a = 2;
 var b = 3;
 
@@ -34,17 +34,17 @@ const class_string = createClassStringObject(component, comp_info, presets);
 const template = await componentDataToTempAST(component, presets);
 const html = htmlTemplateToString(template.html[0]);
 
-assert(component.root_frame.binding_variables.has("a") == true)
-assert(component.root_frame.binding_variables.has("b") == true)
-assert(component.root_frame.binding_variables.has("c") == false)
-assert(component.root_frame.binding_variables.has("x") == false)
-assert(component.root_frame.binding_variables.has("y") == false)
-assert(component.root_frame.binding_variables.has("data") == true)
+assert(component.root_frame.binding_variables.has("a") == true);
+assert(component.root_frame.binding_variables.has("b") == true);
+assert(component.root_frame.binding_variables.has("c") == false);
+assert(component.root_frame.binding_variables.has("x") == false);
+assert(component.root_frame.binding_variables.has("y") == false);
+assert(component.root_frame.binding_variables.has("data") == true);
 
 
-const {hook: hook1} = runClassHookHandlers(component.hooks[0], component, presets, comp_info);
-const {hook: hook2} = runClassHookHandlers(component.hooks[1], component, presets, comp_info);
+const { hook: hook1 } = runClassHookHandlers(component.hooks[0], component, presets, comp_info);
+const { hook: hook2 } = runClassHookHandlers(component.hooks[1], component, presets, comp_info);
 
-assert(renderCompressed(hook1.write_ast) == "this.ct[0].sd(this.model.data.push((a,b,c)=>a+b+c))")
-assert(renderCompressed(hook2.write_ast) == "this.ct[1].sd(this.model.data.push((a,c)=>a+this[1]+c))")
+assert(renderCompressed(hook1.write_ast) == "this.ct[0].sd(this.model.data.push((a,b,c)=>a+b+c))");
+assert(renderCompressed(hook2.write_ast) == "this.ct[1].sd(this.model.data.push((a,c)=>a+this[1]+c))");
 //assert(html == "")
