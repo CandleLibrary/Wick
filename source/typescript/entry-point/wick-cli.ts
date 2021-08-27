797322025776be6fb6fe60ccb69fd405df285c0c
Anthony Weathersby <acweathersby.codes@gmail.com>
2021-08-26 22:01:26 -0600

    WIP: Update CLI entry point

--- source/typescript/entry-point/wick-cli.ts
+++ source/typescript/entry-point/wick-cli.ts
@@ -26,12 +26,20 @@
 	compiled. 
 */
 
-import { addCLIConfig, getPackageJsonObject, getProcessArgs, processCLIConfig } from "@candlelib/paraffin";
-import URL from "@candlelib/uri";
+import {
+	addCLIConfig,
+	getPackageJsonObject,
+	processCLIConfig,
+	traverseFilesFromRoot
+} from "@candlelib/paraffin";
+import URI from '@candlelib/uri';
+import { RenderPage } from '../compiler/ast-render/webpage.js';
+import wick from './wick-server.js';
+import Presets from "../compiler/common/presets.js";
 
 
 const
-	{ package: pkg } = await getPackageJsonObject(new URL(import.meta.url).path),
+	{ package: pkg } = await getPackageJsonObject(new URI(import.meta.url).path),
 	HELP_MESSAGE = `
 CANDLELIB::Wick v${pkg.version}
 
@@ -47,7 +55,7 @@ candle.cure <input-component> <output-directory>
 `;
 
 const IS_FLAT = addCLIConfig("compile", { key: "flat", help_brief: `Output files to a flat directory structure` });
-const output_directory = addCLIConfig("compile", { key: "output", help_brief: `Directory to output files`, REQUIRES_VALUE: true });
+const output_directory = addCLIConfig("compile", { key: "output", help_brief: `Directory in which to output files`, REQUIRES_VALUE: true });
 
 addCLIConfig("compile",
 	{
@@ -55,20 +63,81 @@ addCLIConfig("compile",
 		help_brief:
 			`Statically compile a web application from a source directory.`
 	}
-).callback(
-	async (args) => {
+).callback = (
+		async (args) => {
 
-		//Find all components
-		//Compile
-		//Analyze component information
-		//Extract dependency information and
-		//wherever possible merge into output folder
-		//Identify the root file. 
-		console.log("hello world");
+			//Find all components
+			//Build wick and radiate files 
+			console.log("hello world");
+
+			const fsp = (await import("fs")).default.promises;
+			const page_components = new Map();
+			const components = new Map();
+			const root_directory = new URI(process.cwd());
+			const output_directory = URI.resolveRelative("./www", root_directory + "/");
+			const presets = new Presets();
+
+			console.log({ presets });
+
+			//Find all wick and html files and compile them into components. 
+
+			for await (const file_uri of traverseFilesFromRoot(
+				root_directory, {
+				directory_evaluator_function: (uri) => {
+					const dir_name = uri.path.split("/").pop();
+
+					//Skip linux "hidden" directories
+					if (dir_name[0] == ".")
+						return false;
+
+					//Skip package repo folders
+					if (dir_name == "node_modules")
+						return false;
+
+					return true;
+				}
+			}
+			)) {
+
+				if (["html", "wick"].includes(file_uri.ext.toLowerCase())) {
+
+					const id = file_uri + "";
+
+					if (file_uri.filename == "index")
+						page_components.set(file_uri.dir.slice((root_directory + "").length), id);
+
+					components.set(id, await wick(file_uri));
+
+				} else if ([
+					"jpg", "jpeg", "png", "webp", "svg",
+					"tiff", "ico", "gif", "avif", "bmp",
+				].includes(file_uri.ext.toLowerCase())) {
+					console.log(file_uri + "");
+				} else if ([
+					"css", "js",
+				].includes(file_uri.ext.toLowerCase())) {
+					console.log(file_uri + "");
+				}
+			};
+
+			//Remap dependencies
+
+			for (const [key, val] of page_components) {
+
+				const component = components.get(val);
+
+				const resolved_filepath = URI.resolveRelative("." + key, output_directory + '/');
+
+				await fsp.mkdir(resolved_filepath + "", { recursive: true });
+
+				const { page } = await RenderPage(component, presets);
+
+				await fsp.writeFile(resolved_filepath + "/index.html", page, { encoding: 'utf8' });
+			}
 
 
-		return;
-	}
-);
+			return;
+		}
+	);
 
 processCLIConfig();
